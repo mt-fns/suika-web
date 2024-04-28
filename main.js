0265956d9176517b606624198e0a0737700bd549
Michael Tanto <michaeltanto0909@gmail.com>
2024-04-28 11:28:34 +0700

    Added scoring system

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
-// TODO: make responsive + add score system
-
+// TODO: make responsive
+// fix line path rendering
+// add sounds + change sprites
 
 const colliderScalingFactor = 2.5;
-const spriteScalingFactor = 0.5
+const spriteScalingFactor = 0.5;
 
 let fruits = [];
 let fruitSprites = [];
 let floor;
-let wall_left;
-let wall_right;
+let wallLeft;
+let wallRight;
+let linePath;
+let scoreBoard;
+let score = 0;
+
 let dropped = true;
 let currentFruit;
 
 function dropFruit() {
+    let type = currentFruit.name;
     currentFruit.collider = 'dynamic';
+    updateScore(type);
     dropped = true;
 }
 
 function randomFruit(x, y) {
     if (dropped) {
         let randomValue = Math.random() * 5;
+        // random fruit type
         let randomInt = Math.floor(randomValue);
+
         spawnFruits(x, y, randomInt, true);
-        console.log(currentFruit.name);
         dropped = false;
     }
 }
@@ -34,18 +42,38 @@ function randomFruit(x, y) {
 function spawnFruits(x, y, type, updateCurrent) {
     let fruit = new fruits.Sprite();
 
-    fruit.addAni("default", fruitSprites[type]);
+    fruit.addAni('default', fruitSprites[type]);
     fruit.ani.scale = spriteScalingFactor;
 
-    fruit.name = "type" + type;
+    fruit.name = 'type' + type;
 	fruit.diameter = fruit.ani.width / colliderScalingFactor;
     fruit.y = y;
     fruit.x = x;
 
     if (updateCurrent) {
-        fruit.collider = "none";
+        fruit.collider = 'none';
         currentFruit = fruit;
     }
+}
+
+function createScoreBoard() {
+    scoreBoard = new Sprite();
+    scoreBoard.text = 'Score: 0';
+    scoreBoard.color = '#f7f2c8';
+    scoreBoard.stroke = '#f7f2c8';
+    scoreBoard.textColor = '#000000';
+    scoreBoard.collider = 'none';
+    scoreBoard.textSize = 40;
+    scoreBoard.y = currentFruit.y;
+}
+
+function updateScore(type) {
+    // get last character of string then turn to int
+    let fruitType = +type[type.length - 1];
+    let addScore = (fruitType + 1) * 2;
+    console.log(fruitType);
+    score += addScore;
+    scoreBoard.text = 'Score: ' + score;
 }
 
 function collisionDetector(fruit1, fruit2) {
@@ -53,7 +81,6 @@ function collisionDetector(fruit1, fruit2) {
     let fruitType = +fruit1.name[fruit1.name.length - 1];
 
     if ((fruit1 == currentFruit) | (fruit2 == currentFruit)) {
-        console.log("unallowed collision detected");
         return;
     }
 
@@ -66,13 +93,12 @@ function collisionDetector(fruit1, fruit2) {
         spawnFruits(x, y, newFruitType, false);
         fruit1.remove();
         fruit2.remove();
-        console.log("collision detected");
     }
 }
 
 function setupField() {
     floor = new Sprite();
-	floor.y = 650;
+	floor.y = 750;
 	floor.w = 500;
 	floor.h = 5;
 	floor.collider = 'static';
@@ -80,47 +106,61 @@ function setupField() {
     floor.stroke = '#f6d581';
     floor.strokeWeight = 10;
 
-    wall_left = new Sprite();
-    wall_left.h = 500;
-    wall_left.w = 5;
-    wall_left.y = 400;
-    wall_left.x = 470;
-    wall_left.collider = 'static';
-    wall_left.color = '#f6d581';
-    wall_left.stroke = '#f6d581';
-    wall_left.strokeWeight = 10;
+    wallLeft = new Sprite();
+    wallLeft.h = 500;
+    wallLeft.w = 5;
+    wallLeft.y = 500;
+    wallLeft.x = 470;
+    wallLeft.collider = 'static';
+    wallLeft.color = '#f6d581';
+    wallLeft.stroke = '#f6d581';
+    wallLeft.strokeWeight = 10;
 
-    wall_right = new Sprite();
-    wall_right.h = 500;
-    wall_right.w = 5;
-    wall_right.y = 400;
-    wall_right.x = 470 + floor.w;
-    wall_right.collider = 'static';
-    wall_right.color = '#f6d581';
-    wall_right.stroke = '#f6d581';
-    wall_right.strokeWeight = 10;
+    wallRight = new Sprite();
+    wallRight.h = 500;
+    wallRight.w = 5;
+    wallRight.y = 500;
+    wallRight.x = 470 + floor.w;
+    wallRight.collider = 'static';
+    wallRight.color = '#f6d581';
+    wallRight.stroke = '#f6d581';
+    wallRight.strokeWeight = 10;
+
+    linePath = new Sprite();
+    linePath.h = 500;
+    linePath.w = 2;
+    linePath.y = wallRight.y - currentFruit.diameter;
+    linePath.x = currentFruit.x;
+    linePath.collider = 'none';
+    linePath.color = '#FFFFFF';
+    linePath.stroke = '#FFFFFF';
+
+    createScoreBoard();
 }
 
 function followMouse() {
-    if (mouse.x > wall_right.x) {
-        currentFruit.x = wall_right.x - (wall_right.width + wall_right.strokeWeight);
+    if (mouse.x > wallRight.x) {
+        let newCoords = wallRight.x - (wallRight.width + wallRight.strokeWeight);
+        currentFruit.x = newCoords;
+        linePath.x = newCoords;
     }
-    else if (mouse.x < wall_left.x) {
-        currentFruit.x = wall_left.x + (wall_left.width + wall_left.strokeWeight);
+    else if (mouse.x < wallLeft.x) {
+        let newCoords = wallLeft.x + (wallLeft.width + wallLeft.strokeWeight);
+        currentFruit.x = newCoords;
+        linePath.x = newCoords;
     }
     else {
         currentFruit.x = mouse.x;
-        currentFruit.y = 75;
+        currentFruit.y = 200;
+        
+        linePath.x = mouse.x;
+        linePath.y = wallRight.y - currentFruit.diameter;
     }
-}
-
-function drawLine() {
-    
 }
 
 function preload() {
     for (let i = 0; i < 11; i++) {
-        fruitSprites[i] = loadImage("public/" + i + ".png");
+        fruitSprites[i] = loadImage('public/' + i + '.png');
     }
 }
 
